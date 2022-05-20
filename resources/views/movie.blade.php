e5f4bb43ee4ec7ce4ba3c4783d3baeb95e5ae1bb
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-20 17:26:16 +0200

    da stilare

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -5,15 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Movies</title>
 </head>
 
 <body>
 
     <div class="container">
-        <div class="card">
+        <div class="cards">
             @foreach ($movie as $item)
-                <p>{{ $item['title'] }}</p>
+                <div class="card">{{ $item['title'] }}-{{ $item['original_title'] }} </div>
             @endforeach
         </div>
     </div>
